3992ce5fafe5a6f55099e7d46b7f1d0df252b519
drpsnt <enduro.kexxx@gmail.com>
2023-03-05 23:04:38 +0200

    create homework #2

--- script/index.js
+++ script/index.js
@@ -1,4 +1,32 @@
-//                   HOMEWORK #1
+//                   HOMEWORK #2 (2 промта, выводим результат в консоль)
 
-const name = prompt("Hello! What is your name?", '');
-alert(`Hello,  ${name} ! How are you?`);
+
+// let num1 = prompt('First number');
+// num1 = Number(num1);
+//
+// let num2 = prompt('Second number');
+// num2 = Number(num2);
+//
+// function calc(num1, num2, operator)  {
+//     switch (operator) {
+//         case '-':
+//             result = num1 - num2;
+//             console.log(num1+"-"+num2+"="+result);
+//             break;
+//         case '+':
+//             result = num1 + num2;
+//             console.log(num1+"+"+num2+"="+result);
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             console.log(num1+"*"+num2+"="+result);
+//             break;
+//         case '/':
+//             result = num1 / num2;
+//             console.log(num1+"/"+num2+"="+result);
+//             break;
+//
+//     }
+//     return result;
+// }
+// calc(num1, num2, '+');
