b6b42b5c8cc097a4106637f1197a62a607f7000a
drpsnt <enduro.kexxx@gmail.com>
2023-03-06 14:04:06 +0200

    replacing let with const

--- script/index.js
+++ script/index.js
@@ -5,25 +5,25 @@
 
 //                          Task #1
 
-// let user = {};
-// user.name:"John",
-// user.surname:"Smith"
-// user.name = "Pete";
-// delete user.name;
-// console.log(user);
+ const client = {};
+ client.name = "John";
+ client.surname = "Smith";
+ client.name = "Pete";
+ delete client.name;
+ console.log(client);
 
 
 //                          Task #2
 
 
-// const user = {
-//    name: "John"
-// };
+const user = {
+   name: "John"
+};
 //
-//  это будет работать?
+//   это будет работать?
 //
-// user.name = "Pete";
-// console.log(user);
+user.name = "Pete";
+console.log(user);
 
 //Да, это будет работать. Всё потому что const защищает от изменений только саму переменную user, а не её содержимое.
 
@@ -31,15 +31,15 @@
 //                          Task #3
 
 
-// let salaries = {
-//     John: 100,
-//     Ann: 160,
-//     Pete: 130
-// };
-//
-// let sum = 0;
-// for (let key in salaries) {
-//     sum += salaries[key];
-// }
-//
-// console.log(sum);
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+}
+
+console.log(sum);
