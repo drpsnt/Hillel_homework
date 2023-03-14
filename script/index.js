375824454b7ee0ace89f3e4f6cc6465ac090503c
drpsnt <enduro.kexxx@gmail.com>
2023-03-14 23:50:27 +0200

    create homework #9V2

--- script/index.js
+++ script/index.js
@@ -1,45 +1,114 @@
-//                   HOMEWORK #2 (2 промта, выводим результат в консоль)
+'use strict';
 
+let a = prompt('Введите значение а', '');
 
-const a = +prompt('First number');
+// 1
 
-const b = +prompt('Second number');
+if (a === 0) {
+    alert( 'Верно!' );
+} else {
+    alert( 'Неверно!' );
+}
+// 2
+let message = (a > 0) ? 'Верно!': 'Неверно!';
+alert(message);
 
+// 3
 
-function add(a, b) {
-    return a + b;
+if (a < 0) {
+    alert( 'Верно!' );
+} else {
+    alert( 'Неверно!' );
 }
 
-const resultAdd = add(a,b);
-console.log(`Результат: ${a} + ${b} = ${resultAdd}`);
+// 4
+
+let message = (a >= 0) ? 'Верно!': 'Неверно!';
+alert(message);
+
+// 5
+
+if (a <= 0) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+// 6
+
+let message = (a !== 0) ? 'Верно!': 'Неверно!';
+alert(message);
+
+// 7
+
+if (a === 'test') {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+
+// 8
+
+let message = (a === 1) ? 'Верно!': 'Неверно!';
+alert(message);
+
+// 9
+
+if (a > 0 && a < 5) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+
+// 10
+
+let message = (a === 0 || a === 2) ? a += 7 : a /= 10 ;
+alert(message);
+
+if (+a === 0 || +a === 2) {
+    a = +a + 7;
+} else {
+    a = +a / 10;
+}
+alert(a);
 
 
 
- function sub(a, b) {
-     return a - b;
- }
+// 11
 
-const resultSub = sub(a,b);
- console.log(`Результат: ${a} - ${b} = ${resultSub}`);
+const c = 4;
+const b = 9;
+
+if (c <= 1 && b >= 3) {
+    console.log(c + b);
+} else {
+    console.log(c - b);
+}
+// Результат -5
+
+// 12
+
+if (c > 2 && c < 11 || b >=6 && b < 14) {
+    console.log('Верно!');
+} else {
+    console.log('Неверно!');
+}
+
+// 13
 
 
-
-
-
- function mul(a, b) {
-     return a * b;
- }
-
-const resultMul = mul(a,b);
- console.log(`Результат: ${a} * ${b} = ${resultMul}`);
-
-
-
-
-
- function div(a, b) {
-     return a / b;
- }
-
-const resultDiv = div(a,b);
- console.log(`Результат: ${a} / ${b} = ${resultDiv}`);
+let result;
+switch (num) {
+    case 1: result = 'Зима';
+        break;
+    case 2: result = 'Весна';
+        break;
+    case 3: result = 'Лето';
+        break;
+    case 4: result = 'Осень';
+        break;
+    default : result = 'Неверное значение!';
+}
+console. log(result);
