2dc105e4c4b06d97422b4e1cc30d2df7f5502b3a
drpsnt <enduro.kexxx@gmail.com>
2023-03-15 03:18:35 +0200

    create homework #13

--- script/index.js
+++ script/index.js
@@ -6,16 +6,16 @@ let year;
 
  if (isNaN(number)){
      alert('Error');
- }
- else if ( number[number.length - 1] === '1' ) {
-     year = 'год';
-     alert(`${number} ${year} `);
 
- } else  if ( number[number.length - 2] === '1' && number[number.length - 1] === '2' ||
+ } else  if (number[number.length - 2] === '1' && number[number.length - 1] === '1'|| number[number.length - 2] === '1' && number[number.length - 1] === '2' ||
      number[number.length - 2] === '1' && number[number.length - 1] === '3' ||
      number[number.length - 2] === '1' && number[number.length - 1] === '4'
- ) {
+ )  {
      year = 'лет';
+     alert(`${number} ${year} `);
+ }
+ else if ( number[number.length - 1] === '1') {
+     year = 'год';
      alert(`${number} ${year} `);
 
  } else  if ( number[number.length - 1] === '2' || number[number.length - 1] === '3' || number[number.length - 1] === '4') {
